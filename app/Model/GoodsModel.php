101c5a911ad6579bb61d69fe8d05b5e532ea4590
root <root@localhost.localdomain>
2019-03-26 11:37:21 +0000

    a

---
+++ app/Model/GoodsModel.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2019/3/26
+ * Time: 19:31
+ */
+
+namespace App\Model;
+use Illuminate\Database\Eloquent\Model;
+
+class GoodsModel extends  Model
+{
+    public $table = 'goods';
+    public $timestamps = false;
+}
