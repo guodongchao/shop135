73fd57c8b2bf67587d821bfcaff77cdf7aaf3a93
root <root@localhost.localdomain>
2019-03-27 02:45:57 +0000

    a

--- app/Http/Controllers/Goods/GoodsControllers.php
+++ app/Http/Controllers/Goods/GoodsControllers.php
@@ -17,11 +17,8 @@ class GoodsControllers
     }
     public function showadd(Request $request){
         $id=$request->input('id');
-        echo $id;
-        $data=[
-            'goods_id'=>$id
-        ];
-        $info=GoodsModel::where($data)->get();
-        return $info;
+
+
+        return $id;
     }
 }
